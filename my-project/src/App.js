e27257f1127693b8cfcb418e5c7ff7449d83de11
leramolorova <91638210+leramolorova@users.noreply.github.com>
2022-07-20 04:17:19 +0900

    third

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -27,10 +27,11 @@ function App() {
               Посмотреть кейсы
             </button>
           </div>
-        </div>
-        <div className="w-3/5 bg-red-600 mx-auto">
+          <div className=" bg-red-600">
           <div className="bg-blue-600 text-3xl font-bold py-5">Услуги</div>
 
+
+        <div className="flex">
           <div className="w-1/2 bg-yellow-600 ">
             <div className="font-bold text-lg py-3">Создание сайта</div>
             <div className="pb-3">
@@ -44,10 +45,173 @@ function App() {
               <button className={styles.button}>
                 Разработка интернет-магазина
               </button>
-              <button className={styles.button}>Разработка лендинга</button>
+              <button className={styles.button}>
+                Разработка лендинга
+              </button>
             </div>
           </div>
+          
+          <div className="w-1/2 bg-yellow-200">
+          <div className="font-bold text-lg py-3">Создание сайта</div>
+            <div className="pb-3">
+              Рисуем прототипы, создаём макеты, верстаем для разных типов
+              устройств. Внедряем необходимую CMS, интегрируем CRM.
+            </div>
+            <div>
+              <button className={styles.button}>
+                Разработка корпоративного сайта
+              </button>
+              <button className={styles.button}>
+                Разработка интернет-магазина
+              </button>
+              <button className={styles.button}>
+                Разработка лендинга
+              </button>
+            </div>
+
+          </div>
         </div>
+
+        <div className="flex">
+          <div className="w-1/2 bg-yellow-600 ">
+            <div className="font-bold text-lg py-3">Создание сайта</div>
+            <div className="pb-3">
+              Рисуем прототипы, создаём макеты, верстаем для разных типов
+              устройств. Внедряем необходимую CMS, интегрируем CRM.
+            </div>
+            <div>
+              <button className={styles.button}>
+                Разработка корпоративного сайта
+              </button>
+              <button className={styles.button}>
+                Разработка интернет-магазина
+              </button>
+              <button className={styles.button}>
+                Разработка лендинга
+              </button>
+            </div>
+          </div>
+          
+          <div className="w-1/2 bg-yellow-200">
+          <div className="font-bold text-lg py-3">Создание сайта</div>
+            <div className="pb-3">
+              Рисуем прототипы, создаём макеты, верстаем для разных типов
+              устройств. Внедряем необходимую CMS, интегрируем CRM.
+            </div>
+            <div>
+              <button className={styles.button}>
+                Разработка корпоративного сайта
+              </button>
+              <button className={styles.button}>
+                Разработка интернет-магазина
+              </button>
+              <button className={styles.button}>
+                Разработка лендинга
+              </button>
+            </div>
+
+          </div>
+        </div>
+
+
+
+          </div>
+          <div className="bg-blue-400">
+            <div className="bg-blue-600 text-3xl font-bold py-5">Кейсы</div>
+
+            <div className="h-64 rounded-md bg-black flex mb-5">
+              <div className="w-2/5 bg-green-500 h-full"></div>
+              <div className="bg-orange-500 w-3/5 h-full">
+                <div className="p-5 font-bold text-2xl"> ВТБ</div>
+                <div className="font-medium text-xl ml-5 mb-5"> Маркетплейс билетов на мероприятия</div>
+                <div className="text-sm ml-5 mr-5 mb-5">Дизайн интерфейса / Адаптивная вёрстка
+                                        Проектирование архитектуры БД
+                                        Разработка витрины, модуля арены, модуля внутренней аналитики, личных
+                </div>
+                <button className="rounded-md bg-yellow-500 p-2 ml-5 text-sm">Посмотреть кейс</button>
+
+              </div>
+            </div>
+
+            <div className="h-64 rounded-md bg-black flex mb-5">
+              <div className="w-2/5 bg-green-500 h-full"></div>
+              <div className="bg-orange-500 w-3/5 h-full">
+                <div className="p-5 font-bold text-2xl"> ВТБ</div>
+                <div className="font-medium text-xl ml-5 mb-5"> Маркетплейс билетов на мероприятия</div>
+                <div className="text-sm ml-5 mr-5 mb-5">Дизайн интерфейса / Адаптивная вёрстка
+                                        Проектирование архитектуры БД
+                                        Разработка витрины, модуля арены, модуля внутренней аналитики, личных
+                </div>
+                <button className="rounded-md bg-yellow-500 p-2 ml-5 text-sm">Посмотреть кейс</button>
+
+              </div>
+            </div>
+
+            <div className="h-64 rounded-md bg-black flex mb-5">
+              <div className="w-2/5 bg-green-500 h-full"></div>
+              <div className="bg-orange-500 w-3/5 h-full">
+                <div className="p-5 font-bold text-2xl"> ВТБ</div>
+                <div className="font-medium text-xl ml-5 mb-5"> Маркетплейс билетов на мероприятия</div>
+                <div className="text-sm ml-5 mr-5 mb-5">Дизайн интерфейса / Адаптивная вёрстка
+                                        Проектирование архитектуры БД
+                                        Разработка витрины, модуля арены, модуля внутренней аналитики, личных
+                </div>
+                <button className="rounded-md bg-yellow-500 p-2 ml-5 text-sm">Посмотреть кейс</button>
+
+              </div>
+            </div>
+
+            <button className="rounded-md bg-red-500 text-sm w-full p-2">Остальные работы</button>
+
+          </div>
+          <div className="mb-10">
+          <div className="bg-blue-600 text-3xl font-bold py-5">О нас</div>
+          <div className="bg-green-700 flex">
+            <div className="h-96 bg-white w-1/2 rounded-md"></div>
+            <div className="w-1/2 ml-5 h-96">
+              <div className="flex">
+                <div className="w-1/2 bg-white rounded-md h-48 mr-5"></div>
+                <div className="w-1/2 bg-white rounded-md h-48"></div>
+              </div>
+              <div className="flex">
+                <div className="w-1/2 bg-white rounded-md h-48 mr-5"></div>
+                <div className="w-1/2 bg-white rounded-md h-48"></div>
+              </div>
+            </div>
+          </div>
+          </div>
+          <div className="mb-5">
+            <div className="mb-2 font-medium text-lg mb-5"> Маркетплейс билетов на мероприятия</div>
+            <div className=" text-sm">
+            Maecenas venenatis, tellus sit amet finibus vehicula, felis arcu porttitor metus, eget tempor quam metus lacinia dui. Integer vulputate odio tellus, eget interdum libero mattis eu. Etiam eget dictum orci. Nulla efficitur, erat eu imperdiet mollis, turpis diam aliquam sem, quis laoreet leo metus ut enim. Etiam ac nibh vitae sapien cursus euismod non nec arcu. Morbi molestie eu ante id fringilla. Nam lobortis, ipsum condimentum ornare lobortis, orci erat pulvinar orci, ac facilisis ante dolor non nisi. Ut volutpat augue a lectus sagittis eleifend.
+            </div>
+          </div>
+          <div className="mb-5">
+            <div className="mb-2 font-medium text-lg mb-5"> Маркетплейс билетов на мероприятия</div>
+            <div className=" text-sm">
+            Maecenas venenatis, tellus sit amet finibus vehicula, felis arcu porttitor metus, eget tempor quam metus lacinia dui. Integer vulputate odio tellus, eget interdum libero mattis eu. Etiam eget dictum orci. Nulla efficitur, erat eu imperdiet mollis, turpis diam aliquam sem, quis laoreet leo metus ut enim. Etiam ac nibh vitae sapien cursus euismod non nec arcu. Morbi molestie eu ante id fringilla. Nam lobortis, ipsum condimentum ornare lobortis, orci erat pulvinar orci, ac facilisis ante dolor non nisi. Ut volutpat augue a lectus sagittis eleifend.
+            </div>
+          </div>
+          <div className="text-3xl font-bold mb-5">
+            КОНТАКТЫ
+          </div>
+          <div className="w-full h-64 bg-green-800 rounded-md">
+            <div className="w-2/5 text-center mx-auto pt-10">
+            <div className="text-3xl font-bold">Оставьте заявку</div>
+            <div className="medium font-medium text-lg" > Расскажите о проекте и задайте вопросы - мы скоро ответим.</div>
+            <button className="p-2 bg-white rounded-md text-sm mt-10">Оставьте заявку</button>
+            </div>
+            
+          </div>
+
+          
+
+
+
+
+        </div>
+        
+      
       </div>
     </div>
   );
